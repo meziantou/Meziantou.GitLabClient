7cd21ef6237694051921c1b251973af400052c6f
meziantou <meziantou@live.fr>
2018-09-06 15:47:00 -0400

    add PagedResponse ToListAsync extension method

---
+++ Meziantou.GitLabClient/PageResponseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Meziantou.GitLab
+{
+    public static class PageResponseExtensions
+    {
+        public static async Task<IReadOnlyList<T>> ToListAsync<T>(this Task<PagedResponse<T>> pageResponse)
+            where T : GitLabObject
+        {
+            var page = await pageResponse.ConfigureAwait(false);
+            return await page.ToListAsync().ConfigureAwait(false);
+        }
+    }
+}
